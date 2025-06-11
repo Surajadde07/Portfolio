d1206d38d6387eccf227266771f30453824dceea
suraj <sharadadde@gmail.com>
2025-06-11 09:05:39 +0530

    third-commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, useEffect} from "react";
+import { FC, useEffect } from "react";
 import Button from "@/components/Button";
 import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
 import { useInView } from "motion/react";
@@ -61,18 +61,20 @@ const Footer: FC = () => {
         <div className="grid md:grid-cols-3 md:items-center">
           <div className="md:col-span-2">
             <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight" ref={scope}>Enough talk. Lets&apos;s make something great together.</h2>
-            <Button variant="secondary" className="mt-8" iconAfter={
-              <div className="size-6 overflow-hidden">
-                <div className="w-12 h-6 flex transition-transform duration-300 group-hover/button:-translate-x-1/2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                  </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                  </svg>
+            <a href="mailto:surajadde57@gmail.com?subject=Let's%20Collaborate&body=Hi%20Suraj,%0D%0A%0D%0AI'd%20like%20to%20get%20in%20touch%20with%20you...">
+              <Button variant="secondary" className="mt-8" iconAfter={
+                <div className="size-6 overflow-hidden">
+                  <div className="w-12 h-6 flex transition-transform duration-300 group-hover/button:-translate-x-1/2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                  </div>
                 </div>
-              </div>
-            }>info@surajadde.com</Button>
+              }>info@surajadde.com</Button>
+            </a>
           </div>
           <div>
             <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
